4ccf4295ef322513fea3ae3c7e513be6a6df0c5a
mmmmmiso <misolove7351@naver.com>
2022-04-21 20:26:28 +0900

    addapointment end

---
+++ src/component/AddApointment.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { BiArchive } from "react-icons/bi";
+
+function AddApointment(){
+  return (
+    <div id="appoint">
+        <h4><BiArchive style={{color:'#fff'}}/>예약하기</h4>
+        <ul>
+          <li>
+            <label htmlFor="userName">집사명</label>
+            <input type="text" id="userName"/>
+          </li>
+          <li>
+            <label htmlFor="userChildren">아기명</label>
+            <input type="text" id="userChildren"/>
+          </li>
+          <li>
+            <label htmlFor="userDate">예약일</label>
+            <input type="date" id="userDate"/>
+          </li>
+          <li>
+            <label htmlFor="userTime">예약시간</label>
+            <input type="time" id="userTime"/>
+          </li>
+          <li>
+            <label htmlFor="userDes">기타설명</label>
+            <textarea id="userDes" cols="30" rows="10" placeholder="기타설명"/>
+          </li>
+        </ul>
+        <p><button type="submit">제출</button></p>
+      </div>
+  )
+}
+
+export default AddApointment;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AddApointment />
+  </React.StrictMode>
+);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // component
+import AddApointment from './component/AddApointment.js';
 
 // source
 import './index.css'
+import { BiArchive } from "react-icons/bi";
 
 function App(){
   return (
     <article>
-      welcome
+      <h3><BiArchive style={{color:'#d47776'}}/> 예약시스템</h3>
+      <AddApointment />
+      <div id="list">
+        <ul>
+          <li>반복문</li>
+        </ul>
+      </div>
     </article>
   )
 }
